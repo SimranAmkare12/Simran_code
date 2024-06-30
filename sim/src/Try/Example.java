f98bfbf91d801cd411b8036cc417a6fefc68ba9b
SimranAmkare <simranamkare@gmail.com>
2024-06-30 03:58:17 -0700

    Added new feature

--- sim/src/Try/Example.java
+++ sim/src/Try/Example.java
@@ -5,7 +5,7 @@ public class Example {
 	  int a = 20 ;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-   System.out.println(a);
+    // System.out.println(a);
 	}
 
 }
